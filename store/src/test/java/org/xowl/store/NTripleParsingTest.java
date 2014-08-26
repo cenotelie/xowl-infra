8a48ce03b3c1dbc2e56befc4e4336101ab845799
Laurent Wouters <lwouters@cenotelie.fr>
2014-08-26 18:19:12 +0200

    [tests] Refactored the tests

--- store/src/test/java/org/xowl/store/NTripleParsingTest.java
+++ store/src/test/java/org/xowl/store/NTripleParsingTest.java
@@ -20,348 +20,399 @@
 
 package org.xowl.store;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.xowl.hime.redist.ParseResult;
+import org.xowl.store.loaders.Loader;
+import org.xowl.store.loaders.NTriplesLoader;
+import org.xowl.store.rdf.RDFGraph;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  * Test suite for N-Triples
  *
  * @author Laurent Wouters
  */
-public class NTripleParsingTest extends W3CTestSuite {
+public class NTripleParsingTest {
 
+    /**
+     * The RDF graph for the parsing tests
+     */
+    protected RDFGraph graph;
+
+    /**
+     * Logger for the tests
+     */
+    protected TestLogger logger;
+
+    /**
+     * Initializes this test suite
+     *
+     * @throws IOException
+     */
     public NTripleParsingTest() throws IOException {
+        graph = new RDFGraph();
+        logger = new TestLogger();
+    }
 
+    /**
+     * Tests the parsing of a resource
+     *
+     * @param resource     A resource
+     * @param shallSucceed Whether the loading is expected to succeed
+     */
+    protected void doTest(String resource, boolean shallSucceed) {
+        logger.reset();
+        Loader loader = new NTriplesLoader(graph);
+        InputStream stream = NTripleParsingTest.class.getResourceAsStream(resource);
+        Reader reader = new InputStreamReader(stream);
+
+        ParseResult result = loader.parse(logger, reader);
+        if (shallSucceed) {
+            Assert.assertTrue("Failed to parse resource " + resource, result.isSuccess());
+            Assert.assertEquals("Failed to parse resource " + resource, 0, result.getErrors().size());
+        } else {
+            Assert.assertNotEquals("No error reported while parsing " + resource, 0, result.getErrors().size());
+        }
+
+        try {
+            reader.close();
+        } catch (IOException ex) {
+            Assert.fail("Failed to close the resource " + resource);
+        }
     }
 
     @Test
     public void test_comment_following_triple() throws IOException {
-        test_parsing("/ntriples/comment_following_triple.nt", true);
+        doTest("/ntriples/comment_following_triple.nt", true);
     }
 
     @Test
     public void test_langtagged_string() throws IOException {
-        test_parsing("/ntriples/langtagged_string.nt", true);
+        doTest("/ntriples/langtagged_string.nt", true);
     }
 
     @Test
     public void test_lantag_with_subtag() throws IOException {
-        test_parsing("/ntriples/lantag_with_subtag.nt", true);
+        doTest("/ntriples/lantag_with_subtag.nt", true);
     }
 
     @Test
     public void test_literal() throws IOException {
-        test_parsing("/ntriples/literal.nt", true);
+        doTest("/ntriples/literal.nt", true);
     }
 
     @Test
     public void test_literal_all_controls() throws IOException {
-        test_parsing("/ntriples/literal_all_controls.nt", true);
+        doTest("/ntriples/literal_all_controls.nt", true);
     }
 
     @Test
     public void test_literal_all_punctuation() throws IOException {
-        test_parsing("/ntriples/literal_all_punctuation.nt", true);
+        doTest("/ntriples/literal_all_punctuation.nt", true);
     }
 
     @Test
     public void test_literal_ascii_boundaries() throws IOException {
-        test_parsing("/ntriples/literal_ascii_boundaries.nt", true);
+        doTest("/ntriples/literal_ascii_boundaries.nt", true);
     }
 
     @Test
     public void test_literal_false() throws IOException {
-        test_parsing("/ntriples/literal_false.nt", true);
+        doTest("/ntriples/literal_false.nt", true);
     }
 
     @Test
     public void test_literal_true() throws IOException {
-        test_parsing("/ntriples/literal_true.nt", true);
+        doTest("/ntriples/literal_true.nt", true);
     }
 
     @Test
     public void test_literal_with_2_dquotes() throws IOException {
-        test_parsing("/ntriples/literal_with_2_dquotes.nt", true);
+        doTest("/ntriples/literal_with_2_dquotes.nt", true);
     }
 
     @Test
     public void test_literal_with_2_squotes() throws IOException {
-        test_parsing("/ntriples/literal_with_2_squotes.nt", true);
+        doTest("/ntriples/literal_with_2_squotes.nt", true);
     }
 
     @Test
     public void test_literal_with_BACKSPACE() throws IOException {
-        test_parsing("/ntriples/literal_with_BACKSPACE.nt", true);
+        doTest("/ntriples/literal_with_BACKSPACE.nt", true);
     }
 
     @Test
     public void test_literal_with_CARRIAGE_RETURN() throws IOException {
-        test_parsing("/ntriples/literal_with_CARRIAGE_RETURN.nt", true);
+        doTest("/ntriples/literal_with_CARRIAGE_RETURN.nt", true);
     }
 
     @Test
     public void test_literal_with_CHARACTER_TABULATION() throws IOException {
-        test_parsing("/ntriples/literal_with_CHARACTER_TABULATION.nt", true);
+        doTest("/ntriples/literal_with_CHARACTER_TABULATION.nt", true);
     }
 
     @Test
     public void test_literal_with_dquote() throws IOException {
-        test_parsing("/ntriples/literal_with_dquote.nt", true);
+        doTest("/ntriples/literal_with_dquote.nt", true);
     }
 
     @Test
     public void test_literal_with_FORM_FEED() throws IOException {
-        test_parsing("/ntriples/literal_with_FORM_FEED.nt", true);
+        doTest("/ntriples/literal_with_FORM_FEED.nt", true);
     }
 
     @Test
     public void test_literal_with_LINE_FEED() throws IOException {
-        test_parsing("/ntriples/literal_with_LINE_FEED.nt", true);
+        doTest("/ntriples/literal_with_LINE_FEED.nt", true);
     }
 
     @Test
     public void test_literal_with_numeric_escape4() throws IOException {
-        test_parsing("/ntriples/literal_with_numeric_escape4.nt", true);
+        doTest("/ntriples/literal_with_numeric_escape4.nt", true);
     }
 
     @Test
     public void test_literal_with_numeric_escape8() throws IOException {
-        test_parsing("/ntriples/literal_with_numeric_escape8.nt", true);
+        doTest("/ntriples/literal_with_numeric_escape8.nt", true);
     }
 
     @Test
     public void test_literal_with_REVERSE_SOLIDUS() throws IOException {
-        test_parsing("/ntriples/literal_with_REVERSE_SOLIDUS.nt", true);
+        doTest("/ntriples/literal_with_REVERSE_SOLIDUS.nt", true);
     }
 
     @Test
     public void test_literal_with_REVERSE_SOLIDUS2() throws IOException {
-        test_parsing("/ntriples/literal_with_REVERSE_SOLIDUS2.nt", true);
+        doTest("/ntriples/literal_with_REVERSE_SOLIDUS2.nt", true);
     }
 
     @Test
     public void test_literal_with_squote() throws IOException {
-        test_parsing("/ntriples/literal_with_squote.nt", true);
+        doTest("/ntriples/literal_with_squote.nt", true);
     }
 
     @Test
     public void test_literal_with_UTF8_boundaries() throws IOException {
-        test_parsing("/ntriples/literal_with_UTF8_boundaries.nt", true);
+        doTest("/ntriples/literal_with_UTF8_boundaries.nt", true);
     }
 
     @Test
     public void test_minimal_whitespace() throws IOException {
-        test_parsing("/ntriples/minimal_whitespace.nt", true);
+        doTest("/ntriples/minimal_whitespace.nt", true);
     }
 
     @Test
     public void test_nt_syntax_bad_base_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-base-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-base-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_esc_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-esc-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-esc-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_esc_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-esc-02.nt", false);
+        doTest("/ntriples/nt-syntax-bad-esc-02.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_esc_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-esc-03.nt", false);
+        doTest("/ntriples/nt-syntax-bad-esc-03.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_lang_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-lang-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-lang-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_num_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-num-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-num-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_num_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-num-02.nt", false);
+        doTest("/ntriples/nt-syntax-bad-num-02.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_num_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-num-03.nt", false);
+        doTest("/ntriples/nt-syntax-bad-num-03.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_prefix_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-prefix-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-prefix-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_string_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-string-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-string-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_string_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-string-02.nt", false);
+        doTest("/ntriples/nt-syntax-bad-string-02.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_string_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-string-03.nt", false);
+        doTest("/ntriples/nt-syntax-bad-string-03.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_string_04() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-string-04.nt", false);
+        doTest("/ntriples/nt-syntax-bad-string-04.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_string_05() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-string-05.nt", false);
+        doTest("/ntriples/nt-syntax-bad-string-05.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_string_06() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-string-06.nt", false);
+        doTest("/ntriples/nt-syntax-bad-string-06.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_string_07() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-string-07.nt", false);
+        doTest("/ntriples/nt-syntax-bad-string-07.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_struct_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-struct-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-struct-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_struct_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-struct-02.nt", false);
+        doTest("/ntriples/nt-syntax-bad-struct-02.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_uri_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-uri-01.nt", false);
+        doTest("/ntriples/nt-syntax-bad-uri-01.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_uri_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-uri-02.nt", false);
+        doTest("/ntriples/nt-syntax-bad-uri-02.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_uri_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-uri-03.nt", false);
+        doTest("/ntriples/nt-syntax-bad-uri-03.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_uri_04() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-uri-04.nt", false);
+        doTest("/ntriples/nt-syntax-bad-uri-04.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bad_uri_05() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bad-uri-05.nt", false);
+        doTest("/ntriples/nt-syntax-bad-uri-05.nt", false);
     }
 
     @Test
     public void test_nt_syntax_bnode_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bnode-01.nt", true);
+        doTest("/ntriples/nt-syntax-bnode-01.nt", true);
     }
 
     @Test
     public void test_nt_syntax_bnode_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bnode-02.nt", true);
+        doTest("/ntriples/nt-syntax-bnode-02.nt", true);
     }
 
     @Test
     public void test_nt_syntax_bnode_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-bnode-03.nt", true);
+        doTest("/ntriples/nt-syntax-bnode-03.nt", true);
     }
 
     @Test
     public void test_nt_syntax_datatypes_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-datatypes-01.nt", true);
+        doTest("/ntriples/nt-syntax-datatypes-01.nt", true);
     }
 
     @Test
     public void test_nt_syntax_datatypes_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-datatypes-02.nt", true);
+        doTest("/ntriples/nt-syntax-datatypes-02.nt", true);
     }
 
     @Test
     public void test_nt_syntax_file_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-file-01.nt", true);
+        doTest("/ntriples/nt-syntax-file-01.nt", true);
     }
 
     @Test
     public void test_nt_syntax_file_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-file-02.nt", true);
+        doTest("/ntriples/nt-syntax-file-02.nt", true);
     }
 
     @Test
     public void test_nt_syntax_file_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-file-03.nt", true);
+        doTest("/ntriples/nt-syntax-file-03.nt", true);
     }
 
     @Test
     public void test_nt_syntax_str_esc_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-str-esc-01.nt", true);
+        doTest("/ntriples/nt-syntax-str-esc-01.nt", true);
     }
 
     @Test
     public void test_nt_syntax_str_esc_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-str-esc-02.nt", true);
+        doTest("/ntriples/nt-syntax-str-esc-02.nt", true);
     }
 
     @Test
     public void test_nt_syntax_str_esc_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-str-esc-03.nt", true);
+        doTest("/ntriples/nt-syntax-str-esc-03.nt", true);
     }
 
     @Test
     public void test_nt_syntax_string_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-string-01.nt", true);
+        doTest("/ntriples/nt-syntax-string-01.nt", true);
     }
 
     @Test
     public void test_nt_syntax_string_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-string-02.nt", true);
+        doTest("/ntriples/nt-syntax-string-02.nt", true);
     }
 
     @Test
     public void test_nt_syntax_string_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-string-03.nt", true);
+        doTest("/ntriples/nt-syntax-string-03.nt", true);
     }
 
     @Test
     public void test_nt_syntax_subm_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-subm-01.nt", true);
+        doTest("/ntriples/nt-syntax-subm-01.nt", true);
     }
 
     @Test
     public void test_nt_syntax_uri_01() throws IOException {
-        test_parsing("/ntriples/nt-syntax-uri-01.nt", true);
+        doTest("/ntriples/nt-syntax-uri-01.nt", true);
     }
 
     @Test
     public void test_nt_syntax_uri_02() throws IOException {
-        test_parsing("/ntriples/nt-syntax-uri-02.nt", true);
+        doTest("/ntriples/nt-syntax-uri-02.nt", true);
     }
 
     @Test
     public void test_nt_syntax_uri_03() throws IOException {
-        test_parsing("/ntriples/nt-syntax-uri-03.nt", true);
+        doTest("/ntriples/nt-syntax-uri-03.nt", true);
     }
 
     @Test
     public void test_nt_syntax_uri_04() throws IOException {
-        test_parsing("/ntriples/nt-syntax-uri-04.nt", true);
+        doTest("/ntriples/nt-syntax-uri-04.nt", true);
     }
 }
