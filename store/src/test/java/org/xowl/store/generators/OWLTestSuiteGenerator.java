691f8213186cc2e3e611f00dcee0c48ac879e6bf
Laurent Wouters <lwouters@cenotelie.fr>
2015-03-11 15:14:15 +0100

    [tests] Updated the generator of positive entailment tests

--- store/src/test/java/org/xowl/store/generators/OWLTestSuiteGenerator.java
+++ store/src/test/java/org/xowl/store/generators/OWLTestSuiteGenerator.java
@@ -24,9 +24,10 @@ import org.junit.Test;
 import org.xowl.store.ProxyObject;
 import org.xowl.store.Repository;
 import org.xowl.store.TestLogger;
+import org.xowl.utils.Files;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.io.Writer;
 
 /**
  * Generator for the OWL2 test suite
@@ -52,14 +53,47 @@ public class OWLTestSuiteGenerator {
         repository.load(logger, "http://owl.semanticweb.org/exports/testOntology.rdf");
         repository.load(logger, "http://owl.semanticweb.org/exports/all.rdf");
 
-        ProxyObject classTest = repository.getProxy("http://www.w3.org/2007/OWL/testOntology#TestCase");
         ProxyObject classPositiveEntailment = repository.getProxy("http://www.w3.org/2007/OWL/testOntology#PositiveEntailmentTest");
-        for (ProxyObject test : classTest.getInstances()) {
-            Collection<ProxyObject> types = test.getClassifiers();
-            String id = (String) test.getDataValue("http://www.w3.org/2007/OWL/testOntology#identifier");
-            if (types.contains(classPositiveEntailment)) {
-                System.out.println(id);
-            }
+        for (ProxyObject test : classPositiveEntailment.getInstances()) {
+            generatePositiveEntailmentTest(test);
+        }
+    }
+
+    /**
+     * Generates a positive entailment test from the specified specification
+     *
+     * @param test A test specification
+     */
+    private void generatePositiveEntailmentTest(ProxyObject test) {
+        String id = (String) test.getDataValue("http://www.w3.org/2007/OWL/testOntology#identifier");
+        String name = id.replace("-", "_");
+        System.out.println("@Test public void test_" + name + "() { testPositiveEntailment(\"" + id + "\"); }");
+
+        ProxyObject objSyntax = test.getObjectValue("http://www.w3.org/2007/OWL/testOntology#normativeSyntax");
+        String syntax = null;
+        String premise = null;
+        String conclusion = null;
+        switch (objSyntax.getIRIString()) {
+            case "http://www.w3.org/2007/OWL/testOntology#RDFXML":
+                syntax = "rdf";
+                premise = (String) test.getDataValue("http://www.w3.org/2007/OWL/testOntology#rdfXmlPremiseOntology");
+                conclusion = (String) test.getDataValue("http://www.w3.org/2007/OWL/testOntology#rdfXmlConclusionOntology");
+                break;
+            case "http://www.w3.org/2007/OWL/testOntology#FUNCTIONAL":
+                syntax = "fs";
+                premise = (String) test.getDataValue("http://www.w3.org/2007/OWL/testOntology#fsPremiseOntology");
+                conclusion = (String) test.getDataValue("http://www.w3.org/2007/OWL/testOntology#fsConclusionOntology");
+                break;
+        }
+        try {
+            Writer writer = Files.getWriter("src/test/resources/entailment/" + id + ".premise." + syntax);
+            writer.write(premise);
+            writer.close();
+            writer = Files.getWriter("src/test/resources/entailment/" + id + ".conclusion." + syntax);
+            writer.write(conclusion);
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
