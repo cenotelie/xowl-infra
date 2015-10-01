269c5554eadf605c2ce38afc57b29148c95291ca
Stephen Creff <screff@cenotelie.fr>
2015-10-01 09:20:49 +0200

    Generation method of the tests from RDF added

--- store/src/test/java/org/xowl/store/generators/JSONLDTestSuiteGenerator.java
+++ store/src/test/java/org/xowl/store/generators/JSONLDTestSuiteGenerator.java
@@ -20,6 +20,7 @@
 
 package org.xowl.store.generators;
 
+import org.junit.Test;
 import org.xowl.store.BaseJSONLDTest;
 import org.xowl.store.ProxyObject;
 import org.xowl.store.Repository;
@@ -29,14 +30,17 @@ import org.xowl.store.TestLogger;
  * The generator of the test suite for the JSON-LD syntax
  *
  * @author Laurent Wouters
+ * @author Stephen Creff
  */
 public class JSONLDTestSuiteGenerator {
+
+    @Test
     public void generateToRdfTests() {
         TestLogger logger = new TestLogger();
         Repository repository = new Repository();
         repository.getIRIMapper().addRegexpMap(BaseJSONLDTest.NAMESPACE + "(.*)", "resource://" + BaseJSONLDTest.PHYSICAL + "\\1");
         repository.load(logger, BaseJSONLDTest.NAMESPACE + "tests/toRdf-manifest.jsonld");
-        repository.load(logger, BaseJSONLDTest.NAMESPACE + "tests/normalize-manifest.jsonld");
+        //repository.load(logger, BaseJSONLDTest.NAMESPACE + "tests/normalize-manifest.jsonld");
 
         ProxyObject classRDFTestCase = repository.resolveProxy("http://json-ld.org/test-suite/vocab#ToRDFTest");
         for (ProxyObject test : classRDFTestCase.getInstances()) {
@@ -44,6 +48,23 @@ public class JSONLDTestSuiteGenerator {
             String input = test.getObjectValue("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#action").getIRIString();
             String expect = test.getObjectValue("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#result").getIRIString();
             System.out.println("@Test public void test_toRdf_" + name + "() { toRdfTest(\"" + expect + "\", \"" + input + "\"); }");
+        }
+    }
+
+    @Test
+    public void generateFromRdfTests() {
+        TestLogger logger = new TestLogger();
+        Repository repository  = new Repository();
+
+        repository.getIRIMapper().addRegexpMap(BaseJSONLDTest.NAMESPACE + "(.*)", "resource://" + BaseJSONLDTest.PHYSICAL + "\\1");
+        repository.load(logger, BaseJSONLDTest.NAMESPACE + "tests/fromRdf-manifest.jsonld");
+
+        ProxyObject classRDFTestCase = repository.resolveProxy("http://json-ld.org/test-suite/vocab#FromRDFTest");
+        for (ProxyObject test : classRDFTestCase.getInstances()) {
+            String name = test.getIRIString().substring((BaseJSONLDTest.NAMESPACE + "tests/fromRdf-manifest.jsonld").length() + 1);
+            String input = test.getObjectValue("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#action").getIRIString();
+            String expect = test.getObjectValue("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#result").getIRIString();
+            System.out.println("@Test public void test_fromRdf_" + name + "() { fromRdfTest(\"" + expect + "\", \"" + input + "\"); }");
         }
     }
 }
