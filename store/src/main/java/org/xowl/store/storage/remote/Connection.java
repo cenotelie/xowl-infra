8128fad26af7768cbb005e7233647c70b7573902
Laurent Wouters <lwouters@cenotelie.fr>
2015-11-21 11:50:53 +0100

    [feature] Implementing the remote data store

--- store/src/main/java/org/xowl/store/storage/remote/Connection.java
+++ store/src/main/java/org/xowl/store/storage/remote/Connection.java
@@ -20,16 +20,35 @@
 
 package org.xowl.store.storage.remote;
 
-import org.xowl.store.sparql.Result;
+import org.xowl.hime.redist.ASTNode;
+import org.xowl.hime.redist.ParseError;
+import org.xowl.hime.redist.ParseResult;
+import org.xowl.store.IOUtils;
+import org.xowl.store.IRIs;
+import org.xowl.store.loaders.JSONLDLoader;
+import org.xowl.store.loaders.NQuadsLoader;
+import org.xowl.store.loaders.RDFLoaderResult;
+import org.xowl.store.rdf.Node;
+import org.xowl.store.rdf.QuerySolution;
+import org.xowl.store.rdf.VariableNode;
+import org.xowl.store.sparql.*;
+import org.xowl.store.storage.NodeManager;
+import org.xowl.store.storage.cache.CachedNodes;
+import org.xowl.utils.collections.Couple;
+import org.xowl.utils.logging.BufferedLogger;
+import org.xowl.utils.logging.DispatchLogger;
 import org.xowl.utils.logging.Logger;
 
 import javax.net.ssl.*;
 import java.io.Closeable;
+import java.io.Reader;
+import java.io.StringReader;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.*;
 
 /**
  * Represents a connection to a remote database
@@ -37,6 +56,42 @@ import java.security.cert.X509Certificate;
  * @author Laurent Wouters
  */
 abstract class Connection implements Closeable {
+    /**
+     * An array of SPARQL solutions
+     */
+    private static class ArraySolutions implements Solutions {
+        /**
+         * The content
+         */
+        private final List<QuerySolution> content;
+
+        /**
+         * Initializes the solutions
+         */
+        public ArraySolutions() {
+            content = new ArrayList<>();
+        }
+
+        /**
+         * Adds a new solution to this set
+         *
+         * @param solution The new solution
+         */
+        public void add(QuerySolution solution) {
+            content.add(solution);
+        }
+
+        @Override
+        public int size() {
+            return content.size();
+        }
+
+        @Override
+        public Iterator<QuerySolution> iterator() {
+            return content.iterator();
+        }
+    }
+
     /**
      * The SSL context for HTTPS connections
      */
@@ -101,4 +156,105 @@ abstract class Connection implements Closeable {
      * @return The result
      */
     public abstract Result sparqlUpdate(String command);
+
+
+    /**
+     * Parses a SPARQL response as solutions
+     *
+     * @param content The content to parse
+     * @return The parsed SPARQL result
+     */
+    public static Result parseResponseSolutions(String content) {
+        NodeManager nodeManager = new CachedNodes();
+        JSONLDLoader loader = new JSONLDLoader(nodeManager) {
+            @Override
+            protected Reader getReaderFor(Logger logger, String iri) {
+                return null;
+            }
+        };
+        BufferedLogger bufferedLogger = new BufferedLogger();
+        DispatchLogger dispatchLogger = new DispatchLogger(Logger.DEFAULT, bufferedLogger);
+        ParseResult parseResult = loader.parse(dispatchLogger, new StringReader(content));
+        if (parseResult == null || !parseResult.isSuccess()) {
+            dispatchLogger.error("Failed to parse and load the solutions:");
+            if (parseResult != null) {
+                for (ParseError error : parseResult.getErrors()) {
+                    dispatchLogger.error(error);
+                }
+            }
+            StringBuilder builder = new StringBuilder();
+            for (Object error : bufferedLogger.getErrorMessages()) {
+                builder.append(error.toString());
+                builder.append("\n");
+            }
+            return new ResultFailure(builder.toString());
+        }
+        ASTNode nodeRoot = parseResult.getRoot();
+        Map<String, VariableNode> variables = new HashMap<>();
+        // nodeRoot is the object
+        // root[0] is the 'head' member
+        // root[0][1] is the value of the 'vars' member, which is an array
+        // we iterate over the values in this array
+        for (ASTNode nodeVariable : nodeRoot.getChildren().get(0).getChildren().get(1).getChildren()) {
+            String name = nodeVariable.getValue();
+            variables.put(name, new VariableNode(name));
+        }
+
+        ArraySolutions solutions = new ArraySolutions();
+        // nodeRoot is the object
+        // root[1] is the 'results' member
+        // root[1][1] is the value of the 'results' member, which is an array
+        // we iterate over the values in this array
+        for (ASTNode nodeSolution : nodeRoot.getChildren().get(1).getChildren().get(1).getChildren()) {
+            List<Couple<VariableNode, Node>> bindings = new ArrayList<>();
+            for (ASTNode nodeBinding : nodeSolution.getChildren()) {
+                VariableNode variable = variables.get(nodeBinding.getChildren().get(0).getValue());
+                Node value = IOUtils.deserializeJSON(nodeManager, nodeBinding.getChildren().get(1));
+                bindings.add(new Couple<>(variable, value));
+            }
+            solutions.add(new QuerySolution(bindings));
+        }
+        return new ResultSolutions(solutions);
+    }
+
+    /**
+     * Parses a SPARQL response as quads
+     *
+     * @param content The content to parse
+     * @return The parsed SPARQL result
+     */
+    public static Result parseResponseQuads(String content) {
+        BufferedLogger bufferedLogger = new BufferedLogger();
+        DispatchLogger dispatchLogger = new DispatchLogger(Logger.DEFAULT, bufferedLogger);
+        NQuadsLoader loader = new NQuadsLoader(new CachedNodes());
+        RDFLoaderResult loaderResult = loader.loadRDF(dispatchLogger, new StringReader(content), null, IRIs.GRAPH_DEFAULT);
+        if (loaderResult == null) {
+            dispatchLogger.error("Failed to parse and load the quads");
+            StringBuilder builder = new StringBuilder();
+            for (Object error : bufferedLogger.getErrorMessages()) {
+                builder.append(error.toString());
+                builder.append("\n");
+            }
+            return new ResultFailure(builder.toString());
+        }
+        return new ResultQuads(loaderResult.getQuads());
+    }
+
+    /**
+     * Parses a SPARQL response to an update command
+     *
+     * @param content The content to parse
+     * @return The parsed SPARQL result
+     */
+    public static Result parseResponseUpdate(String content) {
+        if (content == null)
+            return ResultSuccess.INSTANCE;
+        if ("OK".equalsIgnoreCase(content))
+            return ResultSuccess.INSTANCE;
+        if ("true".equalsIgnoreCase(content))
+            return new ResultYesNo(true);
+        if ("false".equalsIgnoreCase(content))
+            return new ResultYesNo(false);
+        return ResultSuccess.INSTANCE;
+    }
 }
--- store/src/main/java/org/xowl/store/storage/remote/HTTPConnection.java
+++ store/src/main/java/org/xowl/store/storage/remote/HTTPConnection.java
@@ -21,24 +21,9 @@
 package org.xowl.store.storage.remote;
 
 import org.apache.xerces.impl.dv.util.Base64;
-import org.xowl.hime.redist.ASTNode;
-import org.xowl.hime.redist.ParseError;
-import org.xowl.hime.redist.ParseResult;
 import org.xowl.store.AbstractRepository;
-import org.xowl.store.IOUtils;
-import org.xowl.store.IRIs;
-import org.xowl.store.loaders.JSONLDLoader;
-import org.xowl.store.loaders.NQuadsLoader;
-import org.xowl.store.loaders.RDFLoaderResult;
-import org.xowl.store.rdf.Node;
-import org.xowl.store.rdf.QuerySolution;
-import org.xowl.store.rdf.VariableNode;
-import org.xowl.store.sparql.*;
-import org.xowl.store.storage.NodeManager;
-import org.xowl.store.storage.cache.CachedNodes;
-import org.xowl.utils.collections.Couple;
-import org.xowl.utils.logging.BufferedLogger;
-import org.xowl.utils.logging.DispatchLogger;
+import org.xowl.store.sparql.Result;
+import org.xowl.store.sparql.ResultFailure;
 import org.xowl.utils.logging.Logger;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -48,7 +33,6 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.*;
 
 /**
  * Manages a connection to a standard remote SPARQL endpoint
@@ -83,41 +67,6 @@ public class HTTPConnection extends Connection {
         public String type;
     }
 
-    /**
-     * An array of SPARQL solutions
-     */
-    private static class ArraySolutions implements Solutions {
-        /**
-         * The content
-         */
-        private final List<QuerySolution> content;
-
-        /**
-         * Initializes the solutions
-         */
-        public ArraySolutions() {
-            content = new ArrayList<>();
-        }
-
-        /**
-         * Adds a new solution to this set
-         *
-         * @param solution The new solution
-         */
-        public void add(QuerySolution solution) {
-            content.add(solution);
-        }
-
-        @Override
-        public int size() {
-            return content.size();
-        }
-
-        @Override
-        public Iterator<QuerySolution> iterator() {
-            return content.iterator();
-        }
-    }
 
     /**
      * URI of the endpoint
@@ -154,72 +103,10 @@ public class HTTPConnection extends Connection {
             return new ResultFailure(response.body != null ? response.body : "failure");
         if (Result.SYNTAX_JSON.equals(response.type)) {
             // solution set
-            NodeManager nodeManager = new CachedNodes();
-            JSONLDLoader loader = new JSONLDLoader(nodeManager) {
-                @Override
-                protected Reader getReaderFor(Logger logger, String iri) {
-                    return null;
-                }
-            };
-            BufferedLogger bufferedLogger = new BufferedLogger();
-            DispatchLogger dispatchLogger = new DispatchLogger(Logger.DEFAULT, bufferedLogger);
-            ParseResult parseResult = loader.parse(dispatchLogger, new StringReader(response.body));
-            if (parseResult == null || !parseResult.isSuccess()) {
-                dispatchLogger.error("Failed to parse and load the solutions:");
-                if (parseResult != null) {
-                    for (ParseError error : parseResult.getErrors()) {
-                        dispatchLogger.error(error);
-                    }
-                }
-                StringBuilder builder = new StringBuilder();
-                for (Object error : bufferedLogger.getErrorMessages()) {
-                    builder.append(error.toString());
-                    builder.append("\n");
-                }
-                return new ResultFailure(builder.toString());
-            }
-            ASTNode nodeRoot = parseResult.getRoot();
-            Map<String, VariableNode> variables = new HashMap<>();
-            // nodeRoot is the object
-            // root[0] is the 'head' member
-            // root[0][1] is the value of the 'vars' member, which is an array
-            // we iterate over the values in this array
-            for (ASTNode nodeVariable : nodeRoot.getChildren().get(0).getChildren().get(1).getChildren()) {
-                String name = nodeVariable.getValue();
-                variables.put(name, new VariableNode(name));
-            }
-
-            ArraySolutions solutions = new ArraySolutions();
-            // nodeRoot is the object
-            // root[1] is the 'results' member
-            // root[1][1] is the value of the 'results' member, which is an array
-            // we iterate over the values in this array
-            for (ASTNode nodeSolution : nodeRoot.getChildren().get(1).getChildren().get(1).getChildren()) {
-                List<Couple<VariableNode, Node>> bindings = new ArrayList<>();
-                for (ASTNode nodeBinding : nodeSolution.getChildren()) {
-                    VariableNode variable = variables.get(nodeBinding.getChildren().get(0).getValue());
-                    Node value = IOUtils.deserializeJSON(nodeManager, nodeBinding.getChildren().get(1));
-                    bindings.add(new Couple<>(variable, value));
-                }
-                solutions.add(new QuerySolution(bindings));
-            }
-            return new ResultSolutions(solutions);
+            return parseResponseSolutions(response.body);
         } else if (AbstractRepository.SYNTAX_NQUADS.equals(response.type)) {
             // quads
-            BufferedLogger bufferedLogger = new BufferedLogger();
-            DispatchLogger dispatchLogger = new DispatchLogger(Logger.DEFAULT, bufferedLogger);
-            NQuadsLoader loader = new NQuadsLoader(new CachedNodes());
-            RDFLoaderResult loaderResult = loader.loadRDF(dispatchLogger, new StringReader(response.body), null, IRIs.GRAPH_DEFAULT);
-            if (loaderResult == null) {
-                dispatchLogger.error("Failed to parse and load the quads");
-                StringBuilder builder = new StringBuilder();
-                for (Object error : bufferedLogger.getErrorMessages()) {
-                    builder.append(error.toString());
-                    builder.append("\n");
-                }
-                return new ResultFailure(builder.toString());
-            }
-            return new ResultQuads(loaderResult.getQuads());
+            return parseResponseQuads(response.body);
         } else {
             // unknown
             return new ResultFailure("Unknown content type " + response.type);
@@ -233,15 +120,7 @@ public class HTTPConnection extends Connection {
             return new ResultFailure("connection failed");
         if (response.code != HttpURLConnection.HTTP_OK)
             return new ResultFailure(response.body != null ? response.body : "failure");
-        if (response.body == null)
-            return ResultSuccess.INSTANCE;
-        if ("OK".equalsIgnoreCase(response.body))
-            return ResultSuccess.INSTANCE;
-        if ("true".equalsIgnoreCase(response.body))
-            return new ResultYesNo(true);
-        if ("false".equalsIgnoreCase(response.body))
-            return new ResultYesNo(false);
-        return ResultSuccess.INSTANCE;
+        return parseResponseUpdate(response.body);
     }
 
     @Override
