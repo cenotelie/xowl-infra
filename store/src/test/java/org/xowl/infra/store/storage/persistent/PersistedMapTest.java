65e8b2a6a26f29977162d434a279a7179ebfc96e
Laurent Wouters <lwouters@cenotelie.fr>
2016-05-29 18:38:04 +0200

    [fix] Implementation of the persisted map

--- store/src/test/java/org/xowl/infra/store/storage/persistent/PersistedMapTest.java
+++ store/src/test/java/org/xowl/infra/store/storage/persistent/PersistedMapTest.java
@@ -20,7 +20,10 @@ package org.xowl.infra.store.storage.persistent;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Tests for the persisted map implementation
@@ -28,19 +31,101 @@ import java.io.File;
  * @author Laurent Wouters
  */
 public class PersistedMapTest {
+    /**
+     * Numbers of threads
+     */
+    private static final int THREAD_COUNT = 8;
+    /**
+     * The number of entries to insert
+     */
+    private static final int ENTRIES = 1024;
 
+    /**
+     * Tests the map for simple insertions and atomic replace
+     *
+     * @throws StorageException When an IO operation fails
+     */
     @Test
-    public void testInserts() throws StorageException {
-        try (FileStore store = new FileStore(new File("/home/laurent"), "laurent", false)) {
+    public void testInserts() throws StorageException, IOException {
+        try (FileStore store = new FileStore(Files.createTempDirectory("PersistedMapTest_testInserts").toFile(), "store", false)) {
             PersistedMap map = PersistedMap.create(store);
-            for (int i = 0; i != 1024; i++) {
+            for (int i = 0; i != ENTRIES; i++) {
                 Assert.assertTrue("Failed at " + i, map.tryPut(i, i));
             }
             store.flush();
-            for (int i = 0; i != 1024; i++) {
+            for (int i = 0; i != ENTRIES; i++) {
                 Assert.assertTrue("Failed at " + i, map.compareAndSet(i, i, i + 1));
             }
             store.flush();
+            for (int i = 0; i != ENTRIES; i++) {
+                Assert.assertFalse("Failed at " + i, map.compareAndSet(i, i, i + 1));
+            }
+            for (int i = 0; i != ENTRIES; i++) {
+                Assert.assertEquals("Wrong mapping", i + 1, map.get(i));
+            }
+        }
+    }
+
+    /**
+     * Tests the map for concurrent insertions and atomic replace
+     *
+     * @throws IOException
+     * @throws StorageException When an IO operation fails
+     */
+    @Test
+    public void testConcurrentInserts() throws IOException, StorageException {
+        Collection<Thread> threads = new ArrayList<>();
+        final boolean successes[] = new boolean[THREAD_COUNT];
+        try (final FileStore store = new FileStore(Files.createTempDirectory("PersistedMapTest_testConcurrentInserts").toFile(), "store", false)) {
+            final PersistedMap map = PersistedMap.create(store);
+            for (int i = 0; i != THREAD_COUNT; i++) {
+                final int index = i;
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean success = true;
+                        try {
+                            for (int i = index; i < ENTRIES; i += THREAD_COUNT) {
+                                if (!map.tryPut(i, i)) {
+                                    success = false;
+                                    break;
+                                }
+                            }
+                            for (int i = index; i < ENTRIES; i += THREAD_COUNT) {
+                                if (!map.compareAndSet(i, i, i + 1)) {
+                                    success = false;
+                                    break;
+                                }
+                            }
+                            for (int i = index; i < ENTRIES; i += THREAD_COUNT) {
+                                if (map.compareAndSet(i, i, i + 1)) {
+                                    success = false;
+                                    break;
+                                }
+                            }
+                        } catch (StorageException exception) {
+                            success = false;
+                        }
+                        successes[index] = success;
+                    }
+                }, "Test Thread " + i);
+                threads.add(thread);
+                thread.start();
+            }
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                }
+            }
+            store.flush();
+            for (int i = 0; i != THREAD_COUNT; i++) {
+                Assert.assertTrue(successes[i]);
+            }
+            for (int i = 0; i != ENTRIES; i++) {
+                Assert.assertEquals("Wrong mapping", i + 1, map.get(i));
+            }
         }
     }
 }
