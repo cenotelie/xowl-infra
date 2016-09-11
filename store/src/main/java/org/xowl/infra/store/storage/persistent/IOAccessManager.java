667aa5813312dc3713beac6af31f27c7a5cddc37
Laurent Wouters <lwouters@cenotelie.fr>
2016-09-11 10:50:49 +0200

    [fix] Avoid memory allocation in IO accesses

--- store/src/main/java/org/xowl/infra/store/storage/persistent/IOAccessManager.java
+++ store/src/main/java/org/xowl/infra/store/storage/persistent/IOAccessManager.java
@@ -20,7 +20,7 @@ package org.xowl.infra.store.storage.persistent;
 import org.xowl.infra.utils.logging.Logging;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Manages the concurrent accesses onto a single IO backend
@@ -29,32 +29,6 @@ import java.util.concurrent.atomic.AtomicReference;
  * @author Laurent Wouters
  */
 class IOAccessManager {
-    /**
-     * A pool-able access
-     */
-    private static class Access extends IOAccessOrdered {
-        /**
-         * The manager for this access object
-         */
-        private final IOAccessManager manager;
-
-        /**
-         * Initializes this access
-         *
-         * @param manager The manager for this access object
-         * @param index   The index of this access in its pool
-         */
-        public Access(IOAccessManager manager, int index) {
-            super(index);
-            this.manager = manager;
-        }
-
-        @Override
-        public void close() {
-            manager.onAccessEnd(this);
-        }
-    }
-
     /**
      * The size of the access pool
      */
@@ -75,11 +49,15 @@ class IOAccessManager {
     /**
      * The first free access object in the pool
      */
-    private final AtomicReference<IOAccessOrdered> poolFirst;
+    private final AtomicLong poolFirst;
     /**
-     * The root of the interval tree for the current accesses
+     * The head of the list of live accesses
      */
-    private final AtomicReference<IOAccessOrdered> root;
+    private final IOAccessOrdered listHead;
+    /**
+     * The tail of the list of live accesses
+     */
+    private final IOAccessOrdered listTail;
     /**
      * The total number of accesses
      */
@@ -131,13 +109,243 @@ class IOAccessManager {
     public IOAccessManager(IOBackend backend) {
         this.backend = backend;
         this.pool = new IOAccessOrdered[ACCESSES_POOL_SIZE];
-        this.poolSize = new AtomicInteger(0);
-        this.poolFirst = new AtomicReference<>(null);
-        this.root = new AtomicReference<>(null);
+        this.poolSize = new AtomicInteger(2);
+        this.poolFirst = new AtomicLong(0x00000000FFFFFFFFL);
+        this.listHead = new IOAccessOrdered(this, 0);
+        this.listTail = new IOAccessOrdered(this, 1);
+        this.listHead.next.set(listTail.identifier);
+        this.pool[0] = listHead;
+        this.pool[1] = listTail;
         this.totalAccesses = 0;
         this.totalTries = 0;
         this.statisticsTimestamp = System.nanoTime();
     }
+
+    /**
+     * Gets whether the specified reference is marked
+     *
+     * @param reference A stamped reference
+     * @return Whether the specified reference is marked
+     */
+    private static boolean isMarked(long reference) {
+        return ((reference & 0x0000000100000000L) == 0x0000000100000000L);
+    }
+
+    /**
+     * Gets the identifier of the access in the specified stamped reference
+     *
+     * @param reference A stamped reference
+     * @return The encapsulated identifier
+     */
+    private static int id(long reference) {
+        return (int) (reference & 0xFFFFFFFFL);
+    }
+
+    /**
+     * Gets the marked reference
+     *
+     * @param reference The original reference
+     * @return The marked reference
+     */
+    private static long marked(long reference) {
+        return (0x0000000100000000L | reference);
+    }
+
+    /**
+     * Searches the left and right node in the list for a place to insert the specified access
+     *
+     * @param access The access to be inserted
+     * @return The indices of the left and right nodes
+     */
+    private long listSearchInsert(IOAccessOrdered access) {
+        IOAccessOrdered leftNode = listHead;
+        long leftNodeNext = 0x00000000FFFFFFFFL;
+        IOAccessOrdered rightNode;
+
+        while (true) {
+            // start by the head
+            IOAccessOrdered currentNode = listHead;
+            long currentNodeNext = listHead.next.get();
+
+            // 1: Find leftNode and rightNode so that either
+            do {
+                if (!isMarked(currentNodeNext)) {
+                    leftNode = currentNode;
+                    leftNodeNext = currentNodeNext;
+                }
+                currentNode = pool[id(currentNodeNext)];
+                if (currentNode == listTail)
+                    break;
+                currentNodeNext = currentNode.next.get();
+                if ((access.writable || currentNode.writable) && !isMarked(currentNodeNext) && !access.disjoints(currentNode)) {
+                    // there is a write overlap
+                    return 0xFFFFFFFFFFFFFFFFL;
+                }
+            } while (isMarked(currentNodeNext) || currentNode.location < access.location);
+            rightNode = currentNode;
+
+            // 2: Check nodes are adjacent
+            if (id(leftNodeNext) == rightNode.identifier) {
+                if (rightNode != listTail && isMarked(rightNode.next.get()))
+                    continue;
+                return ((long) (leftNode.identifier) << 32)
+                        | ((long) (rightNode.identifier));
+            }
+
+            // 3: Remove one or more marked nodes
+            IOAccessOrdered toRelease = pool[id(leftNodeNext)];
+            if (leftNode.next.compareAndSet(leftNodeNext, rightNode.identifier)) {
+                // release the removed nodes
+                while (toRelease != rightNode) {
+                    long toReleaseNext = toRelease.next.get();
+                    if (!isMarked(toReleaseNext))
+                        throw new Error("WTF!");
+                    poolReturnAccess(toRelease);
+                    toRelease = pool[id(toReleaseNext)];
+                }
+                if (rightNode != listTail && isMarked(rightNode.next.get()))
+                    continue;
+                return ((long) (leftNode.identifier) << 32)
+                        | ((long) (rightNode.identifier));
+            }
+        }
+    }
+
+    /**
+     * Tries to insert an access into the list of live accesses
+     *
+     * @param access The access to insert
+     * @return Whether the attempt is successful
+     */
+    private boolean listTryInsert(IOAccessOrdered access) {
+        long data = listSearchInsert(access);
+        if (data == 0xFFFFFFFFFFFFFFFFL)
+            // there is an overlap
+            return false;
+        IOAccessOrdered left = pool[((int) (data >> 32))];
+        IOAccessOrdered right = pool[((int) (data & 0xFFFFFFFFL))];
+        // check for overlaps
+        IOAccessOrdered follower = right;
+        while (follower != listTail && follower.location < access.location + access.length) {
+            long followerNext = follower.next.get();
+            if (isMarked(followerNext))
+                return false;
+            if ((access.writable || follower.writable) && !access.disjoints(follower))
+                return false;
+            follower = pool[id(followerNext)];
+        }
+        access.next.set(right.identifier);
+        return left.next.compareAndSet(right.identifier, access.identifier);
+    }
+
+    /**
+     * Inserts an access into the list of live accesses
+     * The method returns only when the access is safely inserted, i.e. there is no blocking access
+     *
+     * @param access The access to insert
+     * @return The number of tries
+     */
+    private int listInsert(IOAccessOrdered access) {
+        int count = 1;
+        while (!listTryInsert(access))
+            count++;
+        return count;
+    }
+
+    /**
+     * Searches the node on the left of the specified access to be removed
+     *
+     * @param access The access to look for (to be removed)
+     * @return The indices of the left and right nodes
+     */
+    private long listSearchRemove(IOAccessOrdered access) {
+        IOAccessOrdered leftNode = listHead;
+        long leftNodeNext = 0x00000000FFFFFFFFL;
+        IOAccessOrdered rightNode;
+
+        while (true) {
+            // start by the head
+            IOAccessOrdered currentNode = listHead;
+            long currentNodeNext = listHead.next.get();
+
+            // 1: Find leftNode and rightNode so that either
+            do {
+                if (!isMarked(currentNodeNext)) {
+                    leftNode = currentNode;
+                    leftNodeNext = currentNodeNext;
+                }
+                currentNode = pool[id(currentNodeNext)];
+                if (currentNode == listTail)
+                    break;
+                currentNodeNext = currentNode.next.get();
+            } while (isMarked(currentNodeNext) || currentNode != access);
+            rightNode = currentNode;
+            if (rightNode != access) {
+                throw new Error("WTF!");
+            }
+
+            // 2: Check nodes are adjacent
+            if (id(leftNodeNext) == rightNode.identifier) {
+                return ((long) (leftNode.identifier) << 32)
+                        | ((long) (rightNode.identifier));
+            }
+
+            // 3: Remove one or more marked nodes
+            IOAccessOrdered toRelease = pool[id(leftNodeNext)];
+            if (leftNode.next.compareAndSet(leftNodeNext, rightNode.identifier)) {
+                // release the removed nodes
+                while (toRelease != rightNode) {
+                    long toReleaseNext = toRelease.next.get();
+                    if (!isMarked(toReleaseNext))
+                        throw new Error("WTF!");
+                    poolReturnAccess(toRelease);
+                    toRelease = pool[id(toReleaseNext)];
+                }
+                return ((long) (leftNode.identifier) << 32)
+                        | ((long) (rightNode.identifier));
+            }
+        }
+    }
+
+    /**
+     * Tries to remove an access from the list of live accesses
+     *
+     * @param access The access to remove
+     * @return Whether the attempt is successful
+     */
+    private boolean listTryRemove(IOAccessOrdered access) {
+        long data = listSearchRemove(access);
+        IOAccessOrdered left = pool[((int) (data >> 32))];
+        IOAccessOrdered right = pool[((int) (data & 0xFFFFFFFFL))];
+        if (right != access)
+            throw new Error("Concurrent removal of the access");
+        long accessNext = access.next.get();
+        if (isMarked(accessNext))
+            throw new Error("Concurrent removal of the access");
+        // mark the node for a delete
+        if (!access.next.compareAndSet(accessNext, marked(accessNext)))
+            return false;
+        // try a simple delete
+        if (left.next.compareAndSet(access.identifier, accessNext))
+            poolReturnAccess(access);
+        else
+            listSearchRemove(access);
+        return true;
+    }
+
+    /**
+     * Removes an access from the list of live accesses
+     *
+     * @param access The access to remove
+     * @return The number of tries
+     */
+    private int listRemove(IOAccessOrdered access) {
+        int count = 1;
+        while (!listTryRemove(access))
+            count++;
+        return count;
+    }
+
 
     /**
      * Gets an access to the associated backend for the specified span
@@ -149,13 +357,13 @@ class IOAccessManager {
      * @throws StorageException When an IO error occurs
      */
     public IOAccess get(int location, int length, boolean writable) throws StorageException {
-        Access access = newAccess();
+        IOAccessOrdered access = newAccess();
         access.setupIOData(location, length, writable);
-        onAccess(IOAccessOrdered.insert(pool, root, access));
+        onAccess(listInsert(access));
         try {
             access.setupIOData(backend.onAccessRequested(access));
         } catch (StorageException exception) {
-            onAccess(IOAccessOrdered.remove(pool, root, access));
+            onAccess(listRemove(access));
             throw exception;
         }
         return access;
@@ -171,10 +379,10 @@ class IOAccessManager {
      * @return The new access, or null if it cannot be obtained
      */
     public IOAccess get(int location, int length, boolean writable, IOElement element) {
-        Access access = newAccess();
+        IOAccessOrdered access = newAccess();
         access.setupIOData(location, length, writable);
         access.setupIOData(element);
-        onAccess(IOAccessOrdered.insert(pool, root, access));
+        onAccess(listInsert(access));
         return access;
     }
 
@@ -183,14 +391,13 @@ class IOAccessManager {
      *
      * @param access The access
      */
-    private void onAccessEnd(Access access) {
+    public void onAccessEnd(IOAccessOrdered access) {
         try {
             backend.onAccessTerminated(access, access.element);
         } catch (StorageException exception) {
             Logging.getDefault().error(exception);
         }
-        onAccess(IOAccessOrdered.remove(pool, root, access));
-        IOAccessOrdered.poolInsert(poolFirst, access);
+        onAccess(listRemove(access));
     }
 
     /**
@@ -198,20 +405,48 @@ class IOAccessManager {
      *
      * @return A free access object
      */
-    private Access newAccess() {
+    private IOAccessOrdered newAccess() {
         while (true) {
             int size = poolSize.get();
-            IOAccessOrdered result = IOAccessOrdered.poolGet(poolFirst);
-            if (result != null)
-                return (Access) result;
+            long firstData = poolFirst.get();
+            int firstId = (int) (firstData & 0xFFFFFFFFL);
+            int firstStamp = (int) (firstData >> 32);
+
+            if (firstId != -1) {
+                IOAccessOrdered result = pool[firstId];
+                int nextId = result.poolNext;
+                long nextData = (((long) (firstStamp + 1)) << 32) | nextId;
+                if (poolFirst.compareAndSet(firstData, nextData))
+                    return result;
+                continue;
+            }
+
             if (size == pool.length)
                 // wait for a free element
                 continue;
+
             if (poolSize.compareAndSet(size, size + 1)) {
-                Access newAccess = new Access(this, size);
+                IOAccessOrdered newAccess = new IOAccessOrdered(this, size);
                 pool[size] = newAccess;
                 return newAccess;
             }
+        }
+    }
+
+    /**
+     * Returns an access to the pool
+     *
+     * @param access The access to return
+     */
+    private void poolReturnAccess(IOAccessOrdered access) {
+        while (true) {
+            long firstData = poolFirst.get();
+            int firstId = (int) (firstData & 0xFFFFFFFFL);
+            int firstStamp = (int) (firstData >> 32);
+            access.poolNext = firstId;
+            long nextData = (((long) (firstStamp + 1)) << 32) | access.identifier;
+            if (poolFirst.compareAndSet(firstData, nextData))
+                return;
         }
     }
 
--- store/src/main/java/org/xowl/infra/store/storage/persistent/IOAccessOrdered.java
+++ store/src/main/java/org/xowl/infra/store/storage/persistent/IOAccessOrdered.java
@@ -18,289 +18,48 @@
 package org.xowl.infra.store.storage.persistent;
 
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Represents a controlled access to an IO backend that can be checked for exclusive use
  *
  * @author Laurent Wouters
  */
-abstract class IOAccessOrdered extends IOAccess {
+class IOAccessOrdered extends IOAccess {
     /**
-     * The special value of the next index when no next value
+     * The parent manager
      */
-    private static final int INDEX_NULL = -1;
+    private final IOAccessManager manager;
     /**
-     * The special value of the next index when representing a hazard
+     * The identifier of this access
      */
-    private static final int INDEX_HAZARD = -2;
-
+    protected final int identifier;
     /**
-     * The value representing a null stamped reference
+     * The marked reference to the next access
+     * - int: the potential mark
+     * - int: the identifier of the successor of this element
+     * If the reference is marked this node (the one owning the reference) is logically deleted
      */
-    private static final long REF_NULL = 0x00000000FFFFFFFFL;
-
+    protected final AtomicLong next;
     /**
-     * The index of this access in its pool
+     * The successor of this element in its current pool
      */
-    private final int index;
-    /**
-     * The next access element, a stamped reference composed of:
-     * - int: index of the next access in the pool
-     * - int: the reference's stamp
-     */
-    private final AtomicLong next;
-    /**
-     * The next free element in the pool
-     */
-    private IOAccessOrdered poolNext;
+    protected int poolNext;
 
     /**
      * Initializes this element
      *
-     * @param index The index of this access in its pool
+     * @param manager    The parent manager
+     * @param identifier The identifier of this access
      */
-    protected IOAccessOrdered(int index) {
-        this.index = index;
-        this.next = new AtomicLong(REF_NULL);
-        this.poolNext = null;
+    protected IOAccessOrdered(IOAccessManager manager, int identifier) {
+        this.manager = manager;
+        this.identifier = identifier;
+        this.next = new AtomicLong(0x00000000FFFFFFFFL);
+        this.poolNext = -1;
     }
 
-    /**
-     * Inserts a new element into a pool
-     *
-     * @param poolFirst The reference to the next free element
-     * @param element   The element to insert in the pool
-     */
-    public static void poolInsert(AtomicReference<IOAccessOrdered> poolFirst, IOAccessOrdered element) {
-        while (true) {
-            // get the current first free element
-            IOAccessOrdered previous = poolFirst.get();
-            // set it as the next for the current element to insert
-            element.poolNext = previous;
-            // atomically replace the old head by the current element to insert
-            if (poolFirst.compareAndSet(previous, element))
-                return;
-        }
-    }
-
-    /**
-     * Gets the next free element from a pool (or null if there is none)
-     *
-     * @param poolFirst The reference to the next free element
-     * @return The next free element from the pool (or null if there is none)
-     */
-    public static IOAccessOrdered poolGet(AtomicReference<IOAccessOrdered> poolFirst) {
-        while (true) {
-            // get the current first free element
-            IOAccessOrdered result = poolFirst.get();
-            if (result == null)
-                // there is none, return null
-                return null;
-            // get the second free element
-            if (poolFirst.compareAndSet(result, result.poolNext))
-                return result;
-        }
-    }
-
-    /**
-     * Inserts a new element in the interval list
-     *
-     * @param pool    The pool of accesses
-     * @param root    The root element
-     * @param element The element to insert
-     * @return The number of tries it took
-     */
-    public static int insert(IOAccessOrdered[] pool, AtomicReference<IOAccessOrdered> root, IOAccessOrdered element) {
-        int tries = 1;
-        while (true) {
-            if (insertFrom(pool, root, element))
-                return tries;
-            tries++;
-        }
-    }
-
-    /**
-     * Tries to insert an element in the interval list
-     * This method may fail due to concurrent operations in the list.
-     *
-     * @param pool    The pool of accesses
-     * @param root    The root to insert from
-     * @param element The element to insert
-     * @return Whether the operation succeeded
-     */
-    private static boolean insertFrom(IOAccessOrdered[] pool, AtomicReference<IOAccessOrdered> root, IOAccessOrdered element) {
-        IOAccessOrdered current = root.get();
-        long currentNextRef;
-        IOAccessOrdered insertAfter = null;
-        long insertAfterNextRef = REF_NULL;
-        IOAccessOrdered insertBefore = current;
-
-        while (current != null) {
-            // get the next access
-            currentNextRef = current.next.get();
-            int currentNextIndex = getReferenceIndex(currentNextRef);
-            if (currentNextIndex == INDEX_HAZARD)
-                // the current element is marked, fail here to retry
-                return false;
-            // shall we insert before the current element?
-            if (element.location + element.length <= current.location)
-                // yes, the element to insert is completely before the current element
-                break;
-            // not completely before the current element
-            // is there an exclusivity overlap?
-            if ((current.writable || element.writable) && !current.disjoints(element)) {
-                // there is an overlap between the current element and the one to insert
-                // and either one are exclusive
-                return false;
-            }
-            if (currentNextIndex == INDEX_NULL) {
-                // no next access element
-                if (insertAfter == null) {
-                    // there is no next element and the insertAfter has still not be set (will be inserted as last)
-                    insertAfter = current;
-                    insertAfterNextRef = currentNextRef;
-                    insertBefore = null;
-                }
-                current = null;
-            } else {
-                // there is a next
-                IOAccessOrdered next = pool[currentNextIndex];
-                if (element.location < next.location) {
-                    // the element to insert must be before the next one
-                    insertAfter = current;
-                    insertAfterNextRef = currentNextRef;
-                    insertBefore = next;
-                }
-                current = next;
-            }
-        }
-
-        // do the insert
-        // setup the element to insert
-        element.next.set(getUpdatedStampedReference(element.next.get(), insertBefore));
-        // no element to insert after?
-        if (insertAfter == null)
-            // insert as root
-            return root.compareAndSet(current, element);
-        // insert after the selected element
-        return insertAfter.next.compareAndSet(insertAfterNextRef, getUpdatedStampedReference(insertAfterNextRef, element));
-    }
-
-    /**
-     * Removes an element from the interval list
-     *
-     * @param pool    The pool of accesses
-     * @param root    The root element
-     * @param element The element to remove
-     * @return The number of tries it took
-     */
-    public static int remove(IOAccessOrdered[] pool, AtomicReference<IOAccessOrdered> root, IOAccessOrdered element) {
-        int tries = 1;
-        while (true) {
-            if (removeFrom(pool, root, element))
-                return tries;
-            tries++;
-        }
-    }
-
-    /**
-     * Tries to removes an element from the interval list
-     * This method may fail due to concurrent operations in the list.
-     *
-     * @param pool    The pool of accesses
-     * @param root    The root element
-     * @param element The element to remove
-     * @return Whether the operation succeeded
-     */
-    private static boolean removeFrom(IOAccessOrdered[] pool, AtomicReference<IOAccessOrdered> root, IOAccessOrdered element) {
-        IOAccessOrdered current = root.get();
-
-        // is the element to remove the root?
-        if (current == element) {
-            long nextRef = element.next.get();
-            int nextIndex = getReferenceIndex(nextRef);
-            // mark the element for delete
-            long hazard = getUpdatedStampedReference(nextRef, INDEX_HAZARD);
-            if (!element.next.compareAndSet(nextRef, hazard))
-                // concurrent insertion after this element
-                return false;
-            // replace the root
-            IOAccessOrdered next = nextIndex == INDEX_NULL ? null : pool[nextIndex];
-            if (root.compareAndSet(element, next))
-                // success!
-                return true;
-            // failed to replace, current insertion at the root
-            // un-mark the node and retry
-            element.next.compareAndSet(hazard, nextRef);
-            return false;
-        }
-
-        // not the first one, go down the list
-        while (current != null) {
-            long nextRef = current.next.get();
-            int nextIndex = getReferenceIndex(nextRef);
-            if (nextIndex == INDEX_HAZARD)
-                // this is a concurrent delete ...
-                return false;
-            IOAccessOrdered next = nextIndex == INDEX_NULL ? null : pool[nextIndex];
-            if (next == element) {
-                // the next element is the one to be deleted
-                long elementNextRef = element.next.get();
-                int elementNextIndex = getReferenceIndex(elementNextRef);
-                IOAccessOrdered elementNext = elementNextIndex == INDEX_NULL ? null : pool[elementNextIndex];
-                // mark the element for delete
-                long hazard = getUpdatedStampedReference(elementNextRef, INDEX_HAZARD);
-                if (!element.next.compareAndSet(elementNextRef, hazard))
-                    // concurrent insertion after this element
-                    return false;
-                // attempt to delete
-                if (current.next.compareAndSet(nextRef, getUpdatedStampedReference(nextRef, elementNext)))
-                    // success!
-                    return true;
-                // failed to replace, current insertion at the root
-                // un-mark the node and retry
-                element.next.compareAndSet(hazard, elementNextRef);
-                return false;
-            }
-            current = next;
-        }
-        throw new Error("WTF!!");
-    }
-
-    /**
-     * Gets the updated stamped reference
-     *
-     * @param previous The previous value of the stamped reference
-     * @param newValue The new value for the referenced access
-     * @return The updated stamped reference value
-     */
-    private static long getUpdatedStampedReference(long previous, IOAccessOrdered newValue) {
-        return getUpdatedStampedReference(previous, newValue != null ? newValue.index : INDEX_NULL);
-    }
-
-    /**
-     * Gets the updated stamped reference
-     *
-     * @param previous The previous value of the stamped reference
-     * @param index    The updated index for the reference
-     * @return The updated stamped reference val
-     */
-    private static long getUpdatedStampedReference(long previous, int index) {
-        long stamp = (previous >>> 32);
-        stamp++;
-        if (stamp > Integer.MAX_VALUE)
-            stamp = 0;
-        return (stamp << 32) | (0xFFFFFFFFL & (index));
-    }
-
-    /**
-     * Gets the index in the pool of the next access for the specified stamped reference
-     *
-     * @param reference A stamped reference
-     * @return The index of the next access
-     */
-    private static int getReferenceIndex(long reference) {
-        return (int) (reference & 0xFFFFFFFFL);
+    @Override
+    public void close() {
+        manager.onAccessEnd(this);
     }
 }
