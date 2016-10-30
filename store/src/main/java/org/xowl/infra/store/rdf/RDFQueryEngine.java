1ff205993ddf063c6e23991edc53ab4de3dc33b0
Laurent Wouters <lwouters@cenotelie.fr>
2016-10-30 10:43:03 +0100

    [fix] Thread safety for the RDFQueryEngine

--- store/src/main/java/org/xowl/infra/store/rdf/RDFQueryEngine.java
+++ store/src/main/java/org/xowl/infra/store/rdf/RDFQueryEngine.java
@@ -17,8 +17,12 @@
 
 package org.xowl.infra.store.rdf;
 
-import org.xowl.infra.store.rete.*;
+import org.xowl.infra.store.rete.RETENetwork;
+import org.xowl.infra.store.rete.RETERule;
+import org.xowl.infra.store.rete.Token;
+import org.xowl.infra.store.rete.TokenActivable;
 import org.xowl.infra.store.storage.Dataset;
+import org.xowl.infra.utils.collections.ConcurrentListIterator;
 
 import java.util.*;
 
@@ -31,11 +35,7 @@ public class RDFQueryEngine implements ChangeListener {
     /**
      * The number of queries in the cache above which the cache starts to clear itself by removing less used queries
      */
-    private static final int CACHE_CLEAR_THRESHOLD = 300;
-    /**
-     * The number of times a query has been used under which it is susceptible to be dropped from the cache
-     */
-    private static final int CACHE_DROP_THRESHOLD = 5;
+    private static final int CACHE_MAX_SIZE = 64;
 
     /**
      * Represents a cached query that continues being executed
@@ -55,6 +55,7 @@ public class RDFQueryEngine implements ChangeListener {
         private final List<Token> tokens;
         /**
          * The number of times this query has been used
+         * The count is approximate in multi-threading environment but this should be good enough
          */
         private int hitCount;
 
@@ -92,24 +93,30 @@ public class RDFQueryEngine implements ChangeListener {
 
         @Override
         public void activateToken(Token token) {
-            tokens.add(token);
+            synchronized (tokens) {
+                tokens.add(token);
+            }
         }
 
         @Override
         public void deactivateToken(Token token) {
-            tokens.remove(token);
+            synchronized (tokens) {
+                tokens.remove(token);
+            }
         }
 
         @Override
         public void activateTokens(Collection<Token> tokens) {
-            for (Token token : tokens)
-                this.tokens.add(token);
+            synchronized (this.tokens) {
+                this.tokens.addAll(tokens);
+            }
         }
 
         @Override
         public void deactivateTokens(Collection<Token> tokens) {
-            for (Token token : tokens)
-                this.tokens.remove(token);
+            synchronized (this.tokens) {
+                this.tokens.removeAll(tokens);
+            }
         }
 
         /**
@@ -130,118 +137,108 @@ public class RDFQueryEngine implements ChangeListener {
         public List<RDFPatternSolution> getSolutions() {
             hitCount++;
             List<RDFPatternSolution> results = new ArrayList<>();
-            for (Token token : tokens)
-                results.add(new RDFPatternSolution(token.getBindings()));
+            Iterator<Token> iterator = new ConcurrentListIterator<>(tokens);
+            while (iterator.hasNext()) {
+                Token token = iterator.next();
+                if (token != null)
+                    results.add(new RDFPatternSolution(token.getBindings()));
+            }
             return results;
         }
     }
 
     /**
-     * Represents a observed query that continues being executed
+     * Represents the thread-specific inputs and outputs of the engine
      */
-    private static class ObservedElem implements TokenActivable {
+    private static class EngineIO {
         /**
-         * The original query
+         * Buffer of positive quads yet to be flushed
          */
-        private final RDFQuery query;
+        private Collection<Quad> bufferPositives;
         /**
-         * The associated RETE rule
+         * Buffer of negative quads yet ro be flushed
          */
-        private final RETERule rule;
+        private Collection<Quad> bufferNegatives;
         /**
-         * The solution
+         * Flag whether outstanding changes are currently being applied
          */
-        private final Map<Token, RDFPatternSolution> solutions;
-        /**
-         * The observers for this query
-         */
-        private final List<RDFQueryObserver> observers;
+        public boolean isFlushing;
 
         /**
-         * Initializes this observed query
+         * Gets whether there are outstanding changes
          *
-         * @param query    The original query
-         * @param observer The first observer
+         * @return Whether there are outstanding changes
          */
-        public ObservedElem(RDFQuery query, RDFQueryObserver observer) {
-            this.query = query;
-            this.rule = new RETERule(this);
-            this.rule.getPositives().addAll(query.getPositives());
-            this.rule.getNegatives().addAll(query.getNegatives());
-            this.solutions = new HashMap<>();
-            this.observers = new ArrayList<>();
-            this.observers.add(observer);
+        public boolean hasOutstandingChanges() {
+            return (bufferPositives != null && !bufferPositives.isEmpty())
+                    || (bufferNegatives != null && !bufferNegatives.isEmpty());
         }
 
         /**
-         * Gets the RETE rule associated to this query
+         * Gets the positive quads to be injected (and resets the buffer)
          *
-         * @return The associated RETE rule
+         * @return The positive quads to be injected
          */
-        public RETERule getRule() {
-            return rule;
+        public Collection<Quad> checkoutPositivesQuads() {
+            Collection<Quad> result = bufferPositives == null ? Collections.<Quad>emptyList() : bufferPositives;
+            bufferPositives = null;
+            return result;
         }
 
         /**
-         * Adds a new observer
+         * Gets the negative quads to be injected (and resets the buffer)
          *
-         * @param observer The new observer
+         * @return The negative quads to be injected
          */
-        public void addObserver(RDFQueryObserver observer) {
-            observers.add(observer);
-            for (RDFPatternSolution solution : solutions.values()) {
-                observer.onNewSolution(solution);
+        public Collection<Quad> checkoutNegativeQuads() {
+            Collection<Quad> result = bufferNegatives == null ? Collections.<Quad>emptyList() : bufferNegatives;
+            bufferNegatives = null;
+            return result;
+        }
+
+        /**
+         * Adds a quad that is being added
+         *
+         * @param quad The added quad
+         */
+        public void addAddedQuad(Quad quad) {
+            if (bufferPositives == null)
+                bufferPositives = new ArrayList<>();
+            bufferPositives.add(quad);
+        }
+
+        /**
+         * Adds a quad that is being removed
+         *
+         * @param quad The removed quad
+         */
+        public void addRemovedQuad(Quad quad) {
+            if (bufferNegatives == null)
+                bufferNegatives = new ArrayList<>();
+            bufferNegatives.add(quad);
+        }
+
+        /**
+         * Adds a changeset being injected
+         *
+         * @param changeset The injected changeset
+         */
+        public void addChangeset(Changeset changeset) {
+            if (!changeset.getAdded().isEmpty()) {
+                if (bufferPositives == null)
+                    bufferPositives = new ArrayList<>();
+                bufferPositives.addAll(changeset.getAdded());
             }
-        }
-
-        /**
-         * Removes an observer
-         *
-         * @param observer The observer to remove
-         * @return Whether this was the last observer
-         */
-        public boolean removeObserver(RDFQueryObserver observer) {
-            observers.remove(observer);
-            return observers.isEmpty();
-        }
-
-        @Override
-        public void activateToken(Token token) {
-            RDFPatternSolution solution = new RDFPatternSolution(token.getBindings());
-            solutions.put(token, solution);
-            for (RDFQueryObserver observer : observers)
-                observer.onNewSolution(solution);
-        }
-
-        @Override
-        public void deactivateToken(Token token) {
-            RDFPatternSolution solution = solutions.remove(token);
-            if (solution == null)
-                return;
-            for (RDFQueryObserver observer : observers)
-                observer.onSolutionRevoked(solution);
-        }
-
-        @Override
-        public void activateTokens(Collection<Token> tokens) {
-            for (Token token : tokens)
-                activateToken(token);
-        }
-
-        @Override
-        public void deactivateTokens(Collection<Token> tokens) {
-            for (Token token : tokens)
-                deactivateToken(token);
-        }
-
-        /**
-         * Determines whether this cache element matches the specified query
-         *
-         * @param candidate A query candidate
-         * @return true if the candidate matches the query represented by this cache element
-         */
-        public boolean matches(RDFQuery candidate) {
-            return query.equals(candidate);
+            if (!changeset.getDecremented().isEmpty()) {
+                if (bufferNegatives == null)
+                    bufferNegatives = new ArrayList<>();
+                bufferNegatives.addAll(changeset.getDecremented());
+            }
+            if (!changeset.getRemoved().isEmpty()) {
+                if (bufferNegatives == null)
+                    bufferNegatives = new ArrayList<>();
+                bufferNegatives.addAll(changeset.getRemoved());
+            }
         }
     }
 
@@ -251,37 +248,13 @@ public class RDFQueryEngine implements ChangeListener {
      */
     private final RETENetwork rete;
     /**
-     * The new added quads since the last application
-     */
-    private final List<Quad> newAdded;
-    /**
-     * The new removed quads since the last application
-     */
-    private final List<Quad> newRemoved;
-    /**
-     * The new changesets since the last application
-     */
-    private final List<Changeset> newChangesets;
-    /**
-     * Flag whether outstanding changes are currently being applied
-     */
-    private boolean isApplying;
-    /**
-     * Buffer of positive quads
-     */
-    private final Collection<Quad> bufferPositives;
-    /**
-     * Buffer of negative quads
-     */
-    private final Collection<Quad> bufferNegatives;
-    /**
      * The cache of queries
      */
     private final List<CacheElem> cache;
     /**
-     * The observed queries
+     * The thread-specific engine inputs and outputs
      */
-    private final List<ObservedElem> observedQueries;
+    private final ThreadLocal<EngineIO> threadIO;
 
     /**
      * Initializes this engine
@@ -290,14 +263,23 @@ public class RDFQueryEngine implements ChangeListener {
      */
     public RDFQueryEngine(Dataset store) {
         this.rete = new RETENetwork(store);
-        this.newAdded = new ArrayList<>();
-        this.newRemoved = new ArrayList<>();
-        this.newChangesets = new ArrayList<>();
-        this.bufferPositives = new ArrayList<>();
-        this.bufferNegatives = new ArrayList<>();
         this.cache = new ArrayList<>();
-        this.observedQueries = new ArrayList<>();
+        this.threadIO = new ThreadLocal<>();
         store.addListener(this);
+    }
+
+    /**
+     * Gets the thread-specific engine inputs and outputs
+     *
+     * @return The engine inputs and outputs for this thread
+     */
+    private EngineIO getIO() {
+        EngineIO result = threadIO.get();
+        if (result == null) {
+            result = new EngineIO();
+            threadIO.set(result);
+        }
+        return result;
     }
 
     /**
@@ -308,92 +290,41 @@ public class RDFQueryEngine implements ChangeListener {
      */
     public Collection<RDFPatternSolution> execute(RDFQuery query) {
         // try from the cache
-        for (final CacheElem element : cache) {
-            if (element.matches(query)) {
-                Collection<RDFPatternSolution> result = element.getSolutions();
-                // re-sort the cache by hit count (hottest on top)
+        Iterator<CacheElem> iterator = new ConcurrentListIterator<>(cache);
+        while (iterator.hasNext()) {
+            CacheElem element = iterator.next();
+            if (element == null || !element.matches(query))
+                continue;
+            Collection<RDFPatternSolution> result = element.getSolutions();
+            // re-sort the cache by hit count (hottest on top)
+            synchronized (cache) {
                 Collections.sort(cache, new Comparator<CacheElem>() {
                     @Override
                     public int compare(CacheElem element1, CacheElem element2) {
                         return Integer.compare(element2.getHitCount(), element1.getHitCount());
                     }
                 });
-                return result;
             }
+            return result;
         }
 
-        // shall we clear the cache
-        if (cache.size() > CACHE_CLEAR_THRESHOLD) {
-            for (int i = cache.size() - 1; i != -1; i--) {
-                CacheElem element = cache.get(i);
-                if (element.getHitCount() < CACHE_DROP_THRESHOLD) {
-                    rete.removeRule(element.getRule());
-                    cache.remove(i);
-                } else {
-                    // stop here because we go increasing
-                    break;
-                }
+        while (cache.size() >= CACHE_MAX_SIZE) {
+            int index = cache.size() - 1;
+            try {
+                CacheElem toDrop = cache.remove(index);
+                rete.removeRule(toDrop.getRule());
+            } catch (IndexOutOfBoundsException exception) {
+                // ignore
             }
         }
 
         // build the new query and register it in the cache
         CacheElem element = new CacheElem(query);
-        cache.add(element);
+        synchronized (cache) {
+            cache.add(element);
+        }
         rete.addRule(element.getRule());
         return element.getSolutions();
-    }
-
-    /**
-     * Observes the specified query
-     *
-     * @param query    The query to observe
-     * @param observer The observer
-     */
-    public void observe(RDFQuery query, RDFQueryObserver observer) {
-        for (ObservedElem elem : observedQueries) {
-            if (elem.matches(query)) {
-                elem.addObserver(observer);
-                return;
-            }
-        }
-        ObservedElem element = new ObservedElem(query, observer);
-        observedQueries.add(element);
-        rete.addRule(element.getRule());
-    }
-
-    /**
-     * Stops the observation of a query
-     *
-     * @param query    The query to stop observing
-     * @param observer The observer that stops its observation
-     */
-    public void unobserve(RDFQuery query, RDFQueryObserver observer) {
-        for (ObservedElem elem : observedQueries) {
-            if (elem.matches(query)) {
-                if (elem.removeObserver(observer)) {
-                    rete.removeRule(elem.getRule());
-                    observedQueries.remove(elem);
-                }
-                return;
-            }
-        }
-    }
-
-    /**
-     * Gets the matching status of the specified query
-     *
-     * @param query A query
-     * @return The matching status
-     */
-    public MatchStatus getMatchStatus(RDFQuery query) {
-        // try from the cache
-        for (final CacheElem element : cache) {
-            if (element.matches(query)) {
-                return rete.getStatus(element.getRule());
-            }
-        }
-        // the query is not in the cache
-        return null;
     }
 
     @Override
@@ -408,44 +339,35 @@ public class RDFQueryEngine implements ChangeListener {
 
     @Override
     public void onAdded(Quad quad) {
-        newAdded.remove(quad);
-        apply();
+        getIO().addAddedQuad(quad);
+        flush();
     }
 
     @Override
     public void onRemoved(Quad quad) {
-        newRemoved.add(quad);
-        apply();
+        getIO().addRemovedQuad(quad);
+        flush();
     }
 
     @Override
     public void onChange(Changeset changeset) {
-        newChangesets.add(changeset);
-        apply();
+        getIO().addChangeset(changeset);
+        flush();
     }
 
     /**
-     * Applies all outstanding changes
+     * Flushes any outstanding changes in the input or the output
      */
-    protected void apply() {
-        if (isApplying)
+    public void flush() {
+        EngineIO io = getIO();
+        if (io.isFlushing)
             return;
-        isApplying = true;
-        while (!newAdded.isEmpty() || !newRemoved.isEmpty() || !newChangesets.isEmpty()) {
-            bufferPositives.addAll(newAdded);
-            bufferNegatives.addAll(newRemoved);
-            newAdded.clear();
-            newRemoved.clear();
-            for (Changeset changeset : newChangesets) {
-                bufferPositives.addAll(changeset.getAdded());
-                bufferNegatives.addAll(changeset.getRemoved());
-            }
-            newChangesets.clear();
-            rete.injectPositives(bufferPositives);
-            rete.injectNegatives(bufferNegatives);
-            bufferPositives.clear();
-            bufferNegatives.clear();
+        io.isFlushing = true;
+        while (io.hasOutstandingChanges()) {
+            // inject in the RETE network
+            rete.injectPositives(io.checkoutPositivesQuads());
+            rete.injectNegatives(io.checkoutNegativeQuads());
         }
-        isApplying = false;
+        io.isFlushing = false;
     }
 }
