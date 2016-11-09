8bf036106dd11bb4a864f6e07c3795601baad01b
Laurent Wouters <lwouters@cenotelie.fr>
2016-11-09 11:03:07 +0100

    [releng] Updated distribution running scripts

--- .releng/server-linux/install-daemon.sh
+++ .releng/server-linux/install-daemon.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
 SCRIPT="$(readlink -f "$0")"
-DISTRIB="$(dirname $SCRIPT)"
+DISTRIBUTION="$(dirname "$SCRIPT")"
 
 echo "Installing xOWL Server as daemon ..."
-echo "xOWL Server location is $DISTRIB"
+echo "xOWL Server location is $DISTRIBUTION"
 
 rm -f daemon.sh
 touch daemon.sh
 echo "#!/bin/sh" >> daemon.sh
-echo "$DISTRIB/admin.sh \$1" >> daemon.sh
+echo "" >> daemon.sh
+echo "### BEGIN INIT INFO" >> daemon.sh
+echo "# Provides:          xowl-server" >> daemon.sh
+echo "# Required-Start:    $remote_fs $syslog $time" >> daemon.sh
+echo "# Required-Stop:     $remote_fs $syslog $time" >> daemon.sh
+echo "# Default-Start:     2 3 4 5" >> daemon.sh
+echo "# Default-Stop:      0 1 6" >> daemon.sh
+echo "# Short-Description: Daemon for the xOWL Triple Store Server" >> daemon.sh
+echo "# Description:       Daemon for the xOWL Triple Store Server" >> daemon.sh
+echo "### END INIT INFO" >> daemon.sh
+echo "" >> daemon.sh
+echo "$DISTRIBUTION/admin.sh \$1" >> daemon.sh
 
 sudo mv daemon.sh /etc/init.d/xowl-server
 sudo chmod +x /etc/init.d/xowl-server
