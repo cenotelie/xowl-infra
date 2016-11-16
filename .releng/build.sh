eb07589ce39707e6189468088b08eb06aa745fd5
Laurent Wouters <lwouters@cenotelie.fr>
2016-11-16 09:26:31 +0100

    [releng] Updated product manifest generation

--- .releng/build.sh
+++ .releng/build.sh
@@ -7,32 +7,36 @@ ROOT="$(dirname "$RELENG")"
 # Build
 VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -f "$ROOT/pom.xml" -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
 HASH=$(hg -R "$ROOT" --debug id -i)
+TIME=$(date +'%d/%m/%Y %T')
 echo "Building artifacts for version $VERSION ($HASH)"
 mvn clean install -f "$ROOT/pom.xml" -Dgpg.skip=true
 
 # Extract the core products
 cp "$ROOT/server/target/xowl-server-$VERSION-jar-with-dependencies.jar" "$RELENG/xowl-server.jar"
-rm -f "$RELENG/xowl-server.version"
-touch "$RELENG/xowl-server.version"
-echo "version = $VERSION" >> "$RELENG/xowl-server.version"
-echo "changeset = $HASH" >> "$RELENG/xowl-server.version"
+rm -f "$RELENG/xowl-server.manifest"
+touch "$RELENG/xowl-server.manifest"
+echo "version = $VERSION" >> "$RELENG/xowl-server.manifest"
+echo "changeset = $HASH" >> "$RELENG/xowl-server.manifest"
+echo "build-date = $TIME" >> "$RELENG/xowl-server.manifest"
+echo "build-tag = $BUILD_TAG" >> "$RELENG/xowl-server.manifest"
+echo "build-user = $USER" >> "$RELENG/xowl-server.manifest"
 
 # Build the server-linux
 rm -f "$RELENG/xowl-server-$VERSION.tar.gz"
-tar -czf "$RELENG/xowl-server-$VERSION.tar.gz" -C "$ROOT" LICENSE.txt -C "$RELENG" xowl-server.jar xowl-server.version -C "$RELENG/server-linux" xowl-server.ini do-run.sh admin.sh install-daemon.sh uninstall-daemon.sh help.txt
+tar -czf "$RELENG/xowl-server-$VERSION.tar.gz" -C "$ROOT" LICENSE.txt -C "$RELENG" xowl-server.jar xowl-server.manifest -C "$RELENG/server-linux" xowl-server.ini do-run.sh admin.sh install-daemon.sh uninstall-daemon.sh help.txt
 
 # Build the server-docker
 cp "$RELENG/xowl-server.jar" "$RELENG/server-docker/xowl-server.jar"
-cp "$RELENG/xowl-server.version" "$RELENG/server-docker/xowl-server.version"
+cp "$RELENG/xowl-server.manifest" "$RELENG/server-docker/xowl-server.manifest"
 cp "$RELENG/server-linux/xowl-server.ini" "$RELENG/server-docker/xowl-server.ini"
 cp "$RELENG/server-linux/do-run.sh" "$RELENG/server-docker/do-run.sh"
 docker rmi "xowl/xowl-server:$VERSION" || true
 docker build -t "xowl/xowl-server:$VERSION" "$RELENG/server-docker"
 rm "$RELENG/server-docker/xowl-server.jar"
-rm "$RELENG/server-docker/xowl-server.version"
+rm "$RELENG/server-docker/xowl-server.manifest"
 rm "$RELENG/server-docker/xowl-server.ini"
 rm "$RELENG/server-docker/do-run.sh"
 
 # Cleanup
 rm "$RELENG/xowl-server.jar"
-rm "$RELENG/xowl-server.version"
+rm "$RELENG/xowl-server.manifest"
