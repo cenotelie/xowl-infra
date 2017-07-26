67ed205f49890c330fe5c53ff96048d47cee0afc
Laurent Wouters <lwouters@cenotelie.fr>
2017-07-26 16:16:06 +0200

    [feature] Implementing the language server protocol

--- lsp/src/main/java/org/xowl/infra/lsp/structures/ClientCapabilities.java
+++ lsp/src/main/java/org/xowl/infra/lsp/structures/ClientCapabilities.java
@@ -18,9 +18,6 @@
 package org.xowl.infra.lsp.structures;
 
 import fr.cenotelie.hime.redist.ASTNode;
-import org.xowl.infra.utils.Serializable;
-import org.xowl.infra.utils.TextUtils;
-import org.xowl.infra.utils.json.Json;
 import org.xowl.infra.utils.json.JsonDeserializer;
 
 /**
@@ -30,115 +27,25 @@ import org.xowl.infra.utils.json.JsonDeserializer;
  * Servers receiving a ClientCapabilities object literal with unknown properties should ignore these properties.
  * A missing property should be interpreted as an absence of the capability.
  * If a property is missing that defines sub properties all sub properties should be interpreted as an absence of the capability.
+ * TODO: add constants
  *
  * @author Laurent Wouters
  */
-public class ClientCapabilities implements Serializable {
-    /**
-     * Workspace specific client capabilities.
-     */
-    private final WorkspaceClientCapabilities workspace;
-    /**
-     * Text document specific client capabilities.
-     */
-    private final TextDocumentClientCapabilities textDocument;
-    /**
-     * Experimental client capabilities.
-     */
-    private final Object experimental;
-
+public class ClientCapabilities extends Capabilities {
     /**
      * Initializes this structure
      */
     public ClientCapabilities() {
-        this.workspace = new WorkspaceClientCapabilities();
-        this.textDocument = new TextDocumentClientCapabilities();
-        this.experimental = null;
-    }
-
-    /**
-     * Initializes this structure
-     *
-     * @param workspace    Workspace specific client capabilities.
-     * @param textDocument Text document specific client capabilities.
-     * @param experimental Experimental client capabilities.
-     */
-    public ClientCapabilities(
-            WorkspaceClientCapabilities workspace,
-            TextDocumentClientCapabilities textDocument,
-            Object experimental) {
-        this.workspace = workspace;
-        this.textDocument = textDocument;
-        this.experimental = experimental;
+        super();
     }
 
     /**
      * Initializes this structure
      *
      * @param definition   The serialized definition
-     * @param deserializer The current de-serializer
+     * @param deserializer The deserializer to use
      */
     public ClientCapabilities(ASTNode definition, JsonDeserializer deserializer) {
-        WorkspaceClientCapabilities workspace = null;
-        TextDocumentClientCapabilities textDocument = null;
-        Object experimental = null;
-        for (ASTNode child : definition.getChildren()) {
-            ASTNode nodeMemberName = child.getChildren().get(0);
-            String name = TextUtils.unescape(nodeMemberName.getValue());
-            name = name.substring(1, name.length() - 1);
-            ASTNode nodeValue = child.getChildren().get(1);
-            switch (name) {
-                case "workspace": {
-                    workspace = new WorkspaceClientCapabilities(nodeValue, deserializer);
-                    break;
-                }
-                case "textDocument": {
-                    textDocument = new TextDocumentClientCapabilities(nodeValue, deserializer);
-                    break;
-                }
-                case "experimental": {
-                    experimental = deserializer.deserialize(nodeValue, null);
-                    break;
-                }
-            }
-        }
-        this.workspace = workspace;
-        this.textDocument = textDocument;
-        this.experimental = experimental;
-    }
-
-    @Override
-    public String serializedString() {
-        return serializedJSON();
-    }
-
-    @Override
-    public String serializedJSON() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("{");
-        boolean first = true;
-        if (workspace != null) {
-            builder.append("\"workspace\": ");
-            builder.append(workspace.serializedJSON());
-            builder.append("");
-            first = false;
-        }
-        if (textDocument != null) {
-            if (!first)
-                builder.append(", ");
-            builder.append("\"textDocument\": ");
-            builder.append(textDocument.serializedJSON());
-            builder.append("");
-            first = false;
-        }
-        if (experimental != null) {
-            if (!first)
-                builder.append(", ");
-            builder.append("\"experimental\": ");
-            Json.serialize(builder, experimental);
-            builder.append("");
-        }
-        builder.append("}");
-        return builder.toString();
+        super(definition, deserializer);
     }
 }
--- lsp/src/main/java/org/xowl/infra/lsp/structures/ServerCapabilities.java
+++ lsp/src/main/java/org/xowl/infra/lsp/structures/ServerCapabilities.java
@@ -18,7 +18,6 @@
 package org.xowl.infra.lsp.structures;
 
 import fr.cenotelie.hime.redist.ASTNode;
-import org.xowl.infra.utils.TextUtils;
 import org.xowl.infra.utils.json.JsonDeserializer;
 
 /**
@@ -29,40 +28,19 @@ import org.xowl.infra.utils.json.JsonDeserializer;
  */
 public class ServerCapabilities extends Capabilities {
     /**
-     * Experimental server capabilities
-     */
-    private final Object experimental;
-
-    /**
      * Initializes this structure
-     *
-     * @param experimental Experimental server capabilities
      */
-    public ServerCapabilities(Object experimental) {
-        this.experimental = experimental;
+    public ServerCapabilities() {
+        super();
     }
 
     /**
      * Initializes this structure
      *
      * @param definition   The serialized definition
-     * @param deserializer The current de-serializer
+     * @param deserializer The deserializer to use
      */
     public ServerCapabilities(ASTNode definition, JsonDeserializer deserializer) {
         super(definition, deserializer);
-        Object experimental = null;
-        for (ASTNode child : definition.getChildren()) {
-            ASTNode nodeMemberName = child.getChildren().get(0);
-            String name = TextUtils.unescape(nodeMemberName.getValue());
-            name = name.substring(1, name.length() - 1);
-            ASTNode nodeValue = child.getChildren().get(1);
-            switch (name) {
-                case "experimental": {
-                    experimental = deserializer.deserialize(nodeValue, null);
-                    break;
-                }
-            }
-        }
-        this.experimental = experimental;
     }
 }
