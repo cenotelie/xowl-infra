16f32c7f45b1973b814a1163b5fc9c2c8abc2450
Laurent Wouters <lwouters@cenotelie.fr>
2018-06-21 19:12:11 +0200

    [clean] Cleanup store module

--- store/src/test/java/org/xowl/infra/store/entailment/BaseOWLReasoningTest.java
+++ store/src/test/java/org/xowl/infra/store/entailment/BaseOWLReasoningTest.java
@@ -39,71 +39,76 @@ public class BaseOWLReasoningTest {
      * @param premiseResource    The resource for the premise
      * @param conclusionResource The resource for the conclusion
      */
-    protected void testPositiveEntailment(String premiseResource, String conclusionResource) {
-        SinkLogger logger = new SinkLogger();
-
-        // load the conclusion ontology at get all the quads in it
-        RepositoryRDF repository = new RepositoryRDF();
-        // add mapping for imported remote ontologies
-        repository.getIRIMapper().addRegexpMap("http://www.w3.org/2002/03owlt/imports/(.*)", ResourceAccess.SCHEME_RESOURCE + "/org/w3c/imports/\\1.rdf");
-        repository.getIRIMapper().addSimpleMap("http://xowl.org/infra/store/tests/entailment/conclusion", ResourceAccess.SCHEME_RESOURCE + "/org/w3c/entailment/" + conclusionResource);
-        Ontology ontologyConclusion;
-        try {
-            ontologyConclusion = repository.load(logger, "http://xowl.org/infra/store/tests/entailment/conclusion");
-        } catch (Exception exception) {
-            Assert.fail("Some error occurred: " + exception.getMessage());
-            return;
-        }
-        List<Quad> conclusion = new ArrayList<>();
-        try {
-            Iterator<Quad> iterator = repository.getStore().getAll(repository.getGraph(ontologyConclusion));
-            while (iterator.hasNext()) {
-                conclusion.add(iterator.next());
-            }
-            repository.getStore().remove(repository.getGraph(ontologyConclusion), null, null, null);
-        } catch (UnsupportedNodeType ex) {
-            // cannot happen
-        }
-
-        // load the premise ontology and the default ontologies
-        repository.getIRIMapper().addSimpleMap("http://xowl.org/infra/store/tests/entailment/premise", ResourceAccess.SCHEME_RESOURCE + "/org/w3c/entailment/" + premiseResource);
-        try {
-            repository.setEntailmentRegime(EntailmentRegime.OWL2_RDF);
-            repository.load(logger, "http://xowl.org/infra/store/tests/entailment/premise");
-        } catch (Exception exception) {
-            Assert.fail("Some error occurred: " + exception.getMessage());
-            return;
-        }
-        Assert.assertFalse("Some error occurred", logger.isOnError());
-
-        // query the premise for a matching conclusion, modulo the blank nodes
-        RDFQuery query = new RDFQuery();
-        Map<BlankNode, VariableNode> variables = new HashMap<>();
-        for (Quad quad : conclusion) {
-            SubjectNode nodeSubject = quad.getSubject();
-            Property nodeProperty = quad.getProperty();
-            Node nodeObject = quad.getObject();
-            if (nodeSubject.getNodeType() == Node.TYPE_BLANK) {
-                VariableNode variableNode = variables.get(nodeSubject);
-                if (variableNode == null) {
-                    variableNode = new VariableNode(UUID.randomUUID().toString());
-                    variables.put((BlankNode) nodeSubject, variableNode);
+    protected void testPositiveEntailment(String premiseResource, String conclusionResource) throws Exception {
+        final SinkLogger logger = new SinkLogger();
+        try (RepositoryRDF repositoryRdf = new RepositoryRDF()) {
+            repositoryRdf.runAsTransaction((repository, transaction) -> {
+                // load the conclusion ontology at get all the quads in it
+                // add mapping for imported remote ontologies
+                repository.getIRIMapper().addRegexpMap("http://www.w3.org/2002/03owlt/imports/(.*)", ResourceAccess.SCHEME_RESOURCE + "/org/w3c/imports/\\1.rdf");
+                repository.getIRIMapper().addSimpleMap("http://xowl.org/infra/store/tests/entailment/conclusion", ResourceAccess.SCHEME_RESOURCE + "/org/w3c/entailment/" + conclusionResource);
+                Ontology ontologyConclusion;
+                try {
+                    ontologyConclusion = repository.load(logger, "http://xowl.org/infra/store/tests/entailment/conclusion");
+                } catch (Exception exception) {
+                    Assert.fail("Some error occurred: " + exception.getMessage());
+                    return;
                 }
-                nodeSubject = variableNode;
-            }
-            if (nodeObject.getNodeType() == Node.TYPE_BLANK) {
-                VariableNode variableNode = variables.get(nodeObject);
-                if (variableNode == null) {
-                    variableNode = new VariableNode(UUID.randomUUID().toString());
-                    variables.put((BlankNode) nodeObject, variableNode);
-                }
-                nodeObject = variableNode;
-            }
-            query.getPositives().add(new Quad(null, nodeSubject, nodeProperty, nodeObject));
-        }
 
-        Collection<RDFPatternSolution> solutions = repository.getRDFQueryEngine().execute(query);
-        Assert.assertFalse("Entailment failed", solutions.isEmpty());
+
+                List<Quad> conclusion = new ArrayList<>();
+                try {
+                    Iterator<?extends Quad> iterator = transaction.getDataset().getAll(repository.getGraph(ontologyConclusion));
+                    while (iterator.hasNext()) {
+                        conclusion.add(iterator.next());
+                    }
+                    transaction.getDataset().remove(repository.getGraph(ontologyConclusion), null, null, null);
+                } catch (UnsupportedNodeType ex) {
+                    // cannot happen
+                }
+
+                // load the premise ontology and the default ontologies
+                repository.getIRIMapper().addSimpleMap("http://xowl.org/infra/store/tests/entailment/premise", ResourceAccess.SCHEME_RESOURCE + "/org/w3c/entailment/" + premiseResource);
+                try {
+                    repository.setEntailmentRegime(EntailmentRegime.OWL2_RDF);
+                    repository.load(logger, "http://xowl.org/infra/store/tests/entailment/premise");
+                } catch (Exception exception) {
+                    Assert.fail("Some error occurred: " + exception.getMessage());
+                    return;
+                }
+                Assert.assertFalse("Some error occurred", logger.isOnError());
+
+                // query the premise for a matching conclusion, modulo the blank nodes
+                RDFQuery query = new RDFQuery();
+                Map<BlankNode, VariableNode> variables = new HashMap<>();
+                for (Quad quad : conclusion) {
+                    SubjectNode nodeSubject = quad.getSubject();
+                    Property nodeProperty = quad.getProperty();
+                    Node nodeObject = quad.getObject();
+                    if (nodeSubject.getNodeType() == Node.TYPE_BLANK) {
+                        VariableNode variableNode = variables.get(nodeSubject);
+                        if (variableNode == null) {
+                            variableNode = new VariableNode(UUID.randomUUID().toString());
+                            variables.put((BlankNode) nodeSubject, variableNode);
+                        }
+                        nodeSubject = variableNode;
+                    }
+                    if (nodeObject.getNodeType() == Node.TYPE_BLANK) {
+                        VariableNode variableNode = variables.get(nodeObject);
+                        if (variableNode == null) {
+                            variableNode = new VariableNode(UUID.randomUUID().toString());
+                            variables.put((BlankNode) nodeObject, variableNode);
+                        }
+                        nodeObject = variableNode;
+                    }
+                    query.getPositives().add(new Quad(null, nodeSubject, nodeProperty, nodeObject));
+                }
+
+                Collection<RDFPatternSolution> solutions = repository.getRDFQueryEngine().execute(query);
+                Assert.assertFalse("Entailment failed", solutions.isEmpty());
+                return null;
+            }, true);
+        }
     }
 
     /**
